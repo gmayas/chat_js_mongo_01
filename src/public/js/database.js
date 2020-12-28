2758252c45127d2a3dfde9f76261d7a4e0df4789
Gabiel Maya Sanchez <isc_gmayas@hotmail.com>
2020-12-28 02:42:29 -0500

    tenth commit

--- src/public/js/database.js
+++ src/public/js/database.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env['URIMONGODB'], {
+
+const uri = "mongodb+srv://adminchat:chtjsmgdb1000@cluster0.osjde.mongodb.net/Chat?retryWrites=true&w=majority";
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
   .then(() => console.log('db connected in Cluster0'))
   .catch(err => console.log('db connected error in Cluster0: ', err));
+
 
 /*mongoose.connect('mongodb://localhost/chat', {
     useNewUrlParser: true,
@@ -13,3 +16,14 @@ mongoose.connect(process.env['URIMONGODB'], {
   .then(db => console.log('db connected'))
   .catch(err => console.log(err));*/
 
+/*const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://adminchat:chtjsmgdb1000@cluster0.osjde.mongodb.net/Chat?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+client.then(() => console.log('db connected in cluster'));
+client.connect(err => {
+  console.log('client.connect: ', err);
+  client.close();
+});*/
